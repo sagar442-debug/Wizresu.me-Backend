0da11753300bbc714e51516cd38da2bca0b8fa1d
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-11-14 00:56:38 -0700

    Fixed user deletion from backend

--- api/controller/DeleteUser.js
+++ api/controller/DeleteUser.js
@@ -1,13 +1,13 @@
 const User = require("../models/User");
 
-const Deleteuser = async (req, res) => {
-  const { id } = req.body;
+const Deleteuser = async (id) => {
   try {
-    await connect();
     await User.findOneAndDelete({ clerkId: id });
-    res.status(200).json({ message: "User deleted successfull" });
+    console.log("User deleted successfully");
+    return { success: true, message: "User deleted successfully" };
   } catch (error) {
-    res.status(400).json({ message: "Deletion of user unsuccessful", error });
+    console.error("Error deleting user:", error);
+    return { success: false, message: "Deletion of user unsuccessful", error };
   }
 };
 
