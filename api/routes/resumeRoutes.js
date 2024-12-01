79662a1ee84a9fc318d7f4584ba0538efa3c3df4
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-01 12:21:38 -0700

    Added the route for deleting user

--- api/routes/resumeRoutes.js
+++ api/routes/resumeRoutes.js
@@ -5,11 +5,13 @@ const upload = require("../middleware/MulterMiddleware");
 const verifyUserMiddleware = require("../middleware/VerifyUserMiddleware");
 const getResumeDetails = require("../controller/GetResumeDetails");
 const UpdateResume = require("../controller/UpdateResume");
+const DeleteResume = require("../controller/DeleteResume");
 const router = express.Router();
 
 router.post("/save-resume", saveResume);
 router.post("/save-resume-photo", upload.single("file"), SaveResumePhoto);
 router.get("/detail", getResumeDetails);
-router.put("/update", UpdateResume);
+router.put("/update-resume", UpdateResume);
+router.delete("/delete-resume", DeleteResume);
 
 module.exports = router;
