f9d1584f1b5855cb6dc8c55c5de063b447fe1c9d
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2025-01-09 18:32:10 -0700

    Adding multiple backend functionalities

--- api/index.js
+++ api/index.js
@@ -10,6 +10,9 @@ const Stripe = require("stripe");
 const cors = require("cors");
 const stripeRoute = require("./routes/stripeRoutes");
 const StripeWebhook = require("./controller/StripeWebhook");
+const pdf = require("pdf-parse");
+const multer = require("multer");
+var upload = multer();
 
 dotenv.config();
 
@@ -40,4 +43,25 @@ app.get("/", (req, res) =>
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.json());
+app.use(express.raw());
+app.post("/api/pdf/upload-pdf", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  // Process the file buffer
+  const buff = req.file.buffer;
+
+  pdf(buff)
+    .then((data) => {
+      // Log the extracted text
+      res.send({ pdfText: data.text }); // Send the extracted text in the response
+    })
+    .catch((err) => {
+      console.error("Error processing PDF:", err);
+      res.status(500).json({ message: "Error parsing PDF" });
+    });
 });
